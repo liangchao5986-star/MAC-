6b2edd93134d1881bb252c6be7ccf631f8b1716c
liangchao5986-star <136776543+liangchao5986-star@users.noreply.github.com>
2026-05-15 13:21:20 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+APP = ['部门业务机会分析.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': None,
+    'plist': {
+        'CFBundleName': '部门业务机会分析',
+        'CFBundleDisplayName': '部门业务机会分析',
+        'CFBundleVersion': '1.0',
+        'CFBundleIdentifier': 'com.analysis.sales',
+        'NSHighResolutionCapable': True,
+        'LSUIElement': True,  # 关键：不弹出黑框
+    },
+    'packages': [
+        'pandas', 'numpy', 'matplotlib', 'docx', 'openpyxl',
+        'datetime', 'collections', 're', 'os', 'tkinter'
+    ],
+    'excludes': [
+        'tkinter.test', 'matplotlib.tests', 'numpy.testing',
+        'pandas.tests', 'setuptools', 'pip', 'wheel', 'unittest'
+    ],
+    'optimize': 2,
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILE,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
